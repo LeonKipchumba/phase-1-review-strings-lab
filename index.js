c7193e60d69839cd02a1fcd9d3ad6cc27c65ea2f
leon <LeonKogo20@gmail.com>
2025-03-05 16:15:27 +0300

    completed phase-1-review-strings-lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+    const currentUser = 'Leon Kipchumba';
+    const welcomeMessage = 'Welcome to Flatbook, &{Leon Kipchumba}!';
+    const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+    const shortGreeting = 'Welcome, ${Leon KIpchumba.slice(0, 1)}!';
